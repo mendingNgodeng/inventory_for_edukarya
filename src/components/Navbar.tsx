654b644171e77d369e552c8a80663578294c3d4e
mendingNgodeng <helmitiger7@gmail.com>
2026-03-02 13:42:15 +0700

    CORS error on API

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -110,14 +110,14 @@ const isAuthed = isAuthenticated();
         {/* Search bar - Mobile (collapsible) */}
         <div className="md:hidden py-2 border-t border-gray-100">
           <div className="relative">
-            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
+            {/* <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
             <input
               type="text"
               placeholder="Cari..."
               className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg 
                        focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500
                        text-sm"
-            />
+            /> */}
           </div>
         </div>
       </div>
