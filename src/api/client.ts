cad27c54c3bd90299cad4ebac05fb6ffa41208a1
mendingNgodeng <helmitiger7@gmail.com>
2026-03-05 08:21:52 +0700

    add env variable

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "sonner";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_URL_API;
 
 export const publicClient = axios.create({
   baseURL: BASE_URL,
